33ae8d2bbeababfdc65c3b268d33d177675ad2a5
Yanuarprayoga9 <yanuarprayoga.stu@pnc.ac.id>
2024-12-19 19:56:45 +0700

    first commit

---
+++ addons/library/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+
+from . import book
+from . import loan
+git config --global user.name "Yanuarprayoga9"
+git config --global user.email "yanuarprayoga.stu@pnc.ac.id"
